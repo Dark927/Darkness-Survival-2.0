99b6915d6ed761318ffa526262597e3d6fc504b5
Denis (PC) <denistesla2005@gmail.com>
2026-05-03 02:14:12 +0300

    change enum flags to match shader

--- Assets/Scripts/Materials/DarkMainFX/RendererMode.cs
+++ Assets/Scripts/Materials/DarkMainFX/RendererMode.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 
@@ -20,17 +20,18 @@ namespace Materials.DarkMainFX
         NONE = 0,
         FLASH = 1,
         EMISSION = 2,
-        DISSOLVE = 4,
-        HSVREPLACE = 8,
-        HSVINTERP = 16,
-        HSVMASK = 32,
-        JITTERFREE = 64,
-        TINT = 128,
+        EMISSION_TEX = 4,
+        DISSOLVE = 8,
+        HSVREPLACE = 16,
+        HSVINTERP = 32,
+        HSVMASK = 64,
+        JITTERFREE = 128,
+        TINT = 256,
+        OUTLINE = 512,
 
 
-
+        //TODO:
         FADE = 128,
-        OUTLINE = 128,
         GRADIENT = 128,
         PIXELATE = 256,
         BLUR = 512,
